5b9e29f32886df94107ba3e2eb22c9e965300bc4
Jean-Francois Theroux <failshell@gmail.com>
2011-10-05 14:11:35 -0400

    Switched to dns-ruby from net-dns

--- bored.rb
+++ bored.rb
@@ -1,64 +1,52 @@
 #!/usr/bin/env ruby
 
 require 'colorize'
-require 'net/dns/resolver'
+require 'dnsruby'
 require 'socket'
 require 'trollop'
-require 'whois'
-
-start_time = Time.now
 
 opts = Trollop::options do
     opt :domain, 'Domain to scan', :required => true, :type => String
 end
+puts "Target domain: #{opts[:domain]}".light_blue
 
-puts "Target domain: #{opts[:domain]}"
-# Validating domain thru WHOIS
-print 'Validating domain: ',
+# Create a new resolver
+r = Dnsruby::Resolver.new( :use_tcp => true )
+
+# DNS servers
+puts "*** Testing DNS servers ***".light_green
 begin
-    if Whois.query(opts[:domain]).available?
-        puts 'ERROR: Invalid domain'.red
-        exit 1
+    NS_SERVERS = Hash.new
+    r.query("#{opts[:domain]}", 'NS').answer.each do |ns|
+        NS_SERVERS[ns.nsdname.to_s] = IPSocket.getaddress(ns.nsdname.to_s)
     end
-rescue Whois::ServerNotFound=>e
-    puts "ERROR: #{e}".red
-    exit 1
-rescue Timeout::Error=>e
-    puts "ERROR: #{e}".red
+rescue Dnsruby::NXDomain
+    puts 'Invalid domain. Aborting.'.red
     exit 1
 end
-print "done\n"
-
-print "Fetching DNS information: "
-r = Net::DNS::Resolver.new
-# Fetch DNS servers
-NS_SERVERS = Hash.new
-ns = r.query("#{opts[:domain]}", Net::DNS::NS)
-ns.answer.each do |rr|
-    n = rr.nsdname.chomp('.')
-    NS_SERVERS[n] = IPSocket.getaddress(n)
+z = ''
+NS_SERVERS.sort.each do |ns|
+    puts "#{ns[0]} ".light_yellow + "(#{ns[1]}):".light_blue
+    puts "Attempting a zone transfer:"
+    r.nameserver = ns
+    begin
+        z = r.query("#{opts[:domain]}", 'AXFR')
+        puts "Wow ... that worked. Really?!?!\n".light_yellow
+    rescue Dnsruby::Refused
+            puts "Server refused request\n".red
+    end
 end
+puts "\n"
 
-# Fetch MX servers
+# MX servers
+puts "*** Testing MX servers ***".light_green
 MX_SERVERS = Hash.new
-mx = r.mx("#{opts[:domain]}")
-mx.each do |m|
-    MX_SERVERS[m.exchange] = m.preference
+r.query("#{opts[:domain]}", 'MX').answer.each do |mx|
+    MX_SERVERS[mx.exchange.to_s] = mx.preference
 end
-print "done\n"
-
-# Try to fetch zone
-NS_SERVERS.each do |ns|
-    print "Attempting to do a zone transfer on #{ns[0]}:",
-    r.nameservers = ns[1]
-    r.use_tcp = true
-    z = r.axfr("#{opts[:domain]}")
-    if not z.answer.empty?
-        print "done (wow ... really?!?)\n"
-        break
-    end
-    print "fail\n"
+MX_SERVERS.sort_by{ |k,v| v}.each do |mx|
+    puts "#{mx[0]} ".downcase.light_yellow \
+        + "(#{IPSocket.getaddress(mx[0])})".light_blue \
+        + "(#{mx[1]}): \n".light_blue
 end
-
-# Elapsed time
-puts "Run time: #{Time.now - start_time} seconds"
+puts "\n"
